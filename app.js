dbaec57ee38fae49d996f7f11b2091f4e88f3b23
Paolo Del Rio <pdelrio@csdr.ca>
2021-04-09 20:25:26 -0700

    Added almost all of the endpoints.

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ http.createServer(function (req, res) {
 	let urlData = url.parse(req.url, true);
 	let method = req.method; //get, post, put, delete
 	let requestType = urlData.pathname; //key, user, class, grade
-	let apiKey = urlData.query["apikey"];
+	let apiKey = urlData.query.apikey;
 
 	//Define some headers to accept all kinds of requests.
 	const headers = {
@@ -36,13 +36,43 @@ http.createServer(function (req, res) {
 		return;
 	}
 
-	//Special Path: Key Request / Generation. Can be accessed without key verification. For obvious reasons.
+	//Special Path: User Generation. Can be accessed without an account or a key.
+	if (method == "POST" && requestType == "/user") {
+		 //Get and translate the "send" data.
+		 let item = "";
+		 req.on('data', chunk => {
+			 item += chunk;
+		 });
+		 
+		//Wait for the item to finish building.
+		req.on('end', () => {
+			//Assemble.
+			let json_object = JSON.parse(item);
+			let username = json_object.username;
+			let password = json_object.password; 
+			let fname = json_object.fname;
+			let lname = json_object.lname;
+
+			//Add to the database.
+			let sqlPost = "INSERT INTO user (username, password_hashed, fname, lname) VALUES ('" + username + "', '" + password + "', '" + fname + "', '" + lname + "')";
+			db.query(sqlPost, function(err, result) {
+				if (err != null) {
+					res.end("Post failed.");
+					throw err;
+				}
+
+				res.end("Post succeeded!");
+			});
+		});
+	}
+
+	//Special Path: Key Request / Generation. Can be accessed without key verification.
 	if (method == "GET" && requestType == "/key") {
 		
 		//First, verify the user's password.
 		let notedId = -1;
-		let password = urlData.query["password"];
-		let username = urlData.query["username"];
+		let password = urlData.query.password;
+		let username = urlData.query.username;
 		let passwordCheck = new Promise(function(verSuc, verRip) {
 			let sqlCheck = "SELECT password_hashed, user_id FROM user WHERE username = '" + username + "'";
 			db.query(sqlCheck, function (err, result){
@@ -108,7 +138,7 @@ http.createServer(function (req, res) {
 
 	verification_check.then(
 		function verSuc() {verified = true; console.log("Verification succeeded!~");},
-		function verRip() {console.log("Verification failed...");}
+		function verRip() {res.end(); console.log("Verification failed...");}
 	);
 
 	if (verified) {
@@ -117,17 +147,17 @@ http.createServer(function (req, res) {
 		if (method == "GET") {
 			//First, go select the proper query.
 			let sqlGet = "err";
-			if (requestType == "/user") { //USER: Obtains information on the specified user. Requires USERNAME.
+			if (requestType == "/user") { //USER: Obtains information on the user with the key. Requires nothing special.
 				//Obtain required elements.
-				let username = urlData.query["username"];
-				sqlGet = "SELECT * FROM user WHERE username = '" + username + "'";
-			} else if (requestType == "/class") { //CLASS: Obtains information on the specified class. Requires CLASSNAME.
-				let classname = urlData.query["classname"];
-				sqlGet = "";
-			} else if (requestType == "/grade") { //GRADES: Obtains information on the specified grade. Requires CLASSNAME, GRADENAME.
-				let gradename = urlData.query["gradename"];
-				let classname = urlData.query["classname"];
-				sqlGet = "";
+				sqlGet = "SELECT * FROM user WHERE user_id = " + notedId;
+			} else if (requestType == "/class") { //CLASS: Obtains information on all the classes. Requires nothing special.
+				sqlGet = "SELECT * FROM class WHERE class.user_id = " + notedId;
+			} else if (requestType == "/grade") { //GRADES: Obtains information on grades for a class. Requires CLASSNAME.
+				let classname = urlData.query.classname;
+				sqlGet = "SELECT grade.grade_id, grade.class_id, grade.name, grade.score " + 
+				"FROM grade " +
+				"INNER JOIN class ON grade.class_id = class.class_id " +
+				"WHERE class.user_id = " + notedId + " AND class.name = '" + classname + "'";
 			} else { //Basically a failure state.
 				res.end();
 			}
@@ -136,6 +166,7 @@ http.createServer(function (req, res) {
 			if (sqlGet != "err") {
 				db.query(sqlGet, function (err, result) {
 					if (err) {
+						res.end();
 						throw err;
 					}
 
@@ -145,17 +176,106 @@ http.createServer(function (req, res) {
 			}
 		}
 
-		//POST path: puts data onto the server (3 branches)
-		//users, classes, grades
-		if (method == "POST") {
 
+
+		//POST path: puts data onto the server (3 branches)
+		//classes, grades (user is handled elsewhere)
+		if (method == "POST") {
+			//Get and translate the "send" data.
+			let item = "";
+			req.on('data', chunk => {
+				item += chunk;
+			});
+			
+		   	//Wait for the item to finish building.
+		   	req.on('end', () => {
+			   	//Assemble.
+			   	let json_object = JSON.parse(item);
+
+				//Select the proper query.
+				let sqlPost = "err";
+				if (requestType == "/class") { //CLASS: Posts the class. Requires nothing special.
+					let classname = json_object.name;
+					sqlPost = "INSERT INTO class (name, user_id) VALUES ('" + classname + "', " + notedId + ")";
+				} else if (requestType == "/grade") { //GRADE: Posts the grade. Requires the class to associate it with.
+					let classname = urlData.query.classname;
+					let gradename = json_object.name;
+					let score = json_object.score;
+					sqlPost = "INSERT INTO grade (class_id, name, score) " + //subquery!
+					"SELECT class_id, '" + gradename + "', " + score + " FROM class " +
+					"WHERE name = '" + classname + "' AND user_id = " + notedId;
+				}	
+				
+				if (sqlPost != "err") {
+					//Add to the database.
+					db.query(sqlPost, function(err, result) {
+						if (err != null) {
+							res.end("Post failed.");
+							throw err;
+						}
+						res.end("Post succeeded!");
+					});
+				} else {
+					res.end("Post failed.");
+				}
+		  	});
 		}
+
+
 
 		//PUT path: edits data that's on the server (2 branches)
 		//users, classes, grades
 		if (method == "PUT") {
+			//Get and translate the "send" data.
+			let item = "";
+			req.on('data', chunk => {
+				item += chunk;
+			});
+			
+		   	//Wait for the item to finish building.
+		   	req.on('end', () => {
+			   	//Assemble.
+			   	let json_object = JSON.parse(item);
 
+				//Select the proper query.
+				let sqlPut = "err";
+				if (requestType == "/user") { //USER: Edits the user. Requires nothing special.
+					let username = json_object.username;
+					let password = json_object.password; 
+					let fname = json_object.fname;
+					let lname = json_object.lname;
+					sqlPut = "UPDATE user SET username = '" + username + "', password_hashed = '" +  password + "', fname = '" + fname +  "', lname = '" + lname + "' " +
+					"WHERE user_id = " + notedId;
+				}else if (requestType == "/class") { //CLASS: Edits the class. Requires CLASSNAME.
+					let oldclass = urlData.query.classname;
+					let classname = json_object.name;
+					sqlPut = "UPDATE grade SET name = '" + classname + "' WHERE user_id = " + notedId + " AND name = '" + oldclass + "'";
+				} else if (requestType == "/grade") { //GRADE: Edits the grade. Requires CLASSNAME and GRADENAME. You CANNOT move grades between classes.
+					let classbelong = urlData.query.classname;
+					let oldgrade = urlData.query.gradename;
+					let gradename = json_object.name;
+					let score = json_object.score;
+					sqlPut = "UPDATE grade SET name = '" + gradename + "', score = " + score + " " +
+					"WHERE name = '" + oldgrade + "' AND class_id IN " + 
+					"(SELECT class_id FROM class WHERE user_id = " + notedId + " AND name = '" + classbelong + "')";
+				}	
+				
+				if (sqlPut != "err") {
+					//Add to the database.
+					db.query(sqlPost, function(err, result) {
+						if (err != null) {
+							res.end("Put failed.");
+							throw err;
+						}
+						res.end("Put succeeded!");
+					});
+				} else {
+					res.end("Put failed.");
+				}
+		  	});
 		}
+
+
 
 		//DELETE path: deletes data that's on the server (2 branches)
 		//classes, grades
