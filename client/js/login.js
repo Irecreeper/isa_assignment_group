f105ec8307d9606e2a65be2bb914380c16fb8ad9
Kibum Park <parkkibs@gmail.com>
2021-04-15 12:54:17 -0700

    Updated Files to match files in cPanel

--- client/js/login.js
+++ client/js/login.js
@@ -1,36 +1,27 @@
-const endPointRoot = "https://www.bcitchairheir.com/COMP4537/termproject/API/v1/";
+const endPointRoot = "https://www.bcitchairheir.com/gradebase/";
 const myStorage = window.localStorage;
-const key = myStorage.getItem('apikey');
+
 
 //Authentication Functions
 //AJAX Call to get API key from Server.
-let getKey = (username, password) => {
+let getKey = () => {
+    let username = document.getElementById("login_input_username").value;
+    let password = document.getElementById("login_input_password").value;
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET", endPointRoot + "key?username=" + username + "&password=" + password, true);
     xhttp.send();
     xhttp.onreadystatechange = () => {
-        if(this.readyState == 4 && this.status == 200) {
-            myStorage.setItem('apikey', xhttp.responseText);
-            return true;
-        } else {
-            return false;            
-        }
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText);
+            alert(xhttp.responseText);
+            localStorage.setItem("apikey", xhttp.responseText);
+            window.location.href = "https://www.kparkweb.com/COMP4537/termproject/API/V1/html/mainpage.html";
+        } 
     }
 }
-//AJAX Call to Login With Existing Username and Password.
-let login = () => {
-    let username = document.getElementById("login_input_username").innerHTML;
-    let password = document.getElementById("login_input_password").innerHTML;
-    if(username && password) {
-        let status = getKey(username, password);
-        if(status) {
-            location.href = "https://kparkweb.com/COMP4537/termproject/html/main_page.html"
-        } else {
-            alert("Login Failed!");
-        }        
-    }
-}
+
+
 //Function to redirect to signup page.
 let onClickSignUp = () => {
-    this.window.location = 'https://www.kparkweb.com/COMP4537/termproject/API/V1/html/signup.html';
+    window.location.href = 'https://www.kparkweb.com/COMP4537/termproject/API/V1/html/signup.html';
 }
